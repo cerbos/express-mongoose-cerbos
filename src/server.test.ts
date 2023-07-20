de4451bf04697b6cffc34e60862788d5db8aa44f
Alex Olivier <alex@alexolivier.me>
2023-07-20 14:09:44 +0100

    add tests
    Signed-off-by: Alex Olivier <alex@alexolivier.me>

--- src/server.test.ts
+++ src/server.test.ts
@@ -1,7 +1,7 @@
 import request from "supertest";
 import { app } from "./server";
 import mongoose from "mongoose";
-import { contacts, seedData } from "./seed";
+import { seedData } from "./seed";
 import { IContact } from "./models";
 
 beforeAll(async () => {
@@ -21,14 +21,32 @@ function extractId(c: IContact) {
 }
 
 describe("GET /contacts", function () {
-  it("responds with json", async function () {
+  it("alice", async function () {
     const response = await request(app)
       .get("/contacts")
       .auth("alice", "supersecret")
       .set("Accept", "application/json");
     expect(response.status).toEqual(200);
-    expect(response.body.contacts.map(extractId)).toEqual(
-      contacts.map(extractId)
-    );
+    expect(response.body.contacts.map(extractId)).toEqual([
+      "64b91a27af1d18f54c5cde2b",
+      "64b91a2c51101541e320f892",
+      "64b91a3341475fff928ff83c",
+      "64b91a37a8d29a4728afd661",
+      "64b91a3f8824a9649a0671fe",
+    ]);
+  });
+
+  it("john", async function () {
+    const response = await request(app)
+      .get("/contacts")
+      .auth("john", "password1234")
+      .set("Accept", "application/json");
+    expect(response.status).toEqual(200);
+    expect(response.body.contacts.map(extractId)).toEqual([
+      "64b91a27af1d18f54c5cde2b",
+      "64b91a2c51101541e320f892",
+      "64b91a3341475fff928ff83c",
+      "64b91a3f8824a9649a0671fe",
+    ]);
   });
 });
